dad9843dd0acac1759baeea01e1e4151378589ca
Marwa homsi <marwa.9101@hotmail.com>
2022-05-05 23:50:38 +0200

    component head acceuil a été créé

--- front/src/app/header-home/header-home.component.ts
+++ front/src/app/header-home/header-home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header-home',
+  selector: 'header-home',
   templateUrl: './header-home.component.html',
   styleUrls: ['./header-home.component.scss']
 })
