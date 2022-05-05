182dede448c55b032f114870db2f8f1cd67735f5
Marwa homsi <marwa.9101@hotmail.com>
2022-05-05 23:45:11 +0200

    generate component & html & scss terminés

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderHomeComponent } from './header-home/header-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderHomeComponent
   ],
   imports: [
     BrowserModule,
